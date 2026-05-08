291aeaa18653dd1eb83c0e51568b364b7c7b7562
Danilo Borges Santos <borges_santos89@hotmail.com>
2026-05-08 10:12:32 -0300

    Adiciona macros ao contentpipeline

---
+++ xnpp-content-pipeline/include/Xna/ContentPipeline/Macros.hpp
@@ -0,0 +1,34 @@
+#ifndef XNA_CONTENTPIPELINE_MACROS_HPP
+#define XNA_CONTENTPIPELINE_MACROS_HPP
+
+#define XNPP_DECLARE_NULL_IMPL_WRAPPER(ClassName, Impl) \
+    inline ClassName(std::nullptr_t) { Impl = nullptr; } \
+    inline bool operator==(ClassName const& other) const noexcept { return Impl == other.Impl; } \
+    inline bool operator==(std::nullptr_t) const noexcept { return Impl == nullptr; } \
+    inline explicit operator bool() const noexcept { return Impl != nullptr; }
+
+#define XNPP_DECLARE_NULL_IMPL_BASE_WRAPPER(ClassName, BaseClassName, Impl) \
+    inline ClassName(std::nullptr_t) : BaseClassName(nullptr) { Impl = nullptr; } \
+    inline bool operator==(ClassName const& other) const noexcept { return Impl == other.Impl; } \
+    inline bool operator==(std::nullptr_t) const noexcept { return Impl == nullptr; } \
+    inline explicit operator bool() const noexcept { return Impl != nullptr; }
+
+#ifdef _WIN32
+#ifdef XNPP_STATIC
+// Se for estático, não usamos import/export
+#define XNPP_API 
+#else
+// Se for DLL, verificamos se estamos construindo ou consumindo a DLL
+#ifdef XNPP_EXPORT_DLL
+#define XNPP_API __declspec(dllexport)
+#else
+#define XNPP_API __declspec(dllimport)
+#endif
+#endif
+#else
+// Para Linux/Mac, deixamos vazio ou usamos __attribute__((visibility("default"))))
+#define XNPP_API
+#endif
+
+
+#endif
