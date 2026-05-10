f30c76ee439a2bacc05c03bb7dc94699ca9a49a9
Danilo Borges Santos <borges_santos89@hotmail.com>
2026-05-10 20:43:33 -0300

    Corrige content-cli

--- xnpp-content-cli/source/main.cpp
+++ xnpp-content-cli/source/main.cpp
@@ -1,37 +1,35 @@
 #include <Xna/ContentPipeline/App.hpp>
 #include <Xna/ContentPipeline/BuildTask.hpp>
+#include <iostream>
+#include <filesystem>
+#include <string>
 
 using namespace Xna;
-
-static void ClearConsole() {
-#ifdef _WIN32
-    system("cls");
-#else
-    system("clear");
-#endif
-}
 
 int main(int argc, char* argv[]) {
 
     App::Register();
 
-    std::string caminhoArquivo;
-
-    while (true) {
-        std::cout << "Enter the full file path (ex: D:\\Project\\Project.json): ";
-
-        std::getline(std::cin, caminhoArquivo);
-
-        if (!std::filesystem::exists(caminhoArquivo)) {
-            std::cout << "File not found";
-            continue;
-        }
-
-        auto result = BuildTask::OpenProject(caminhoArquivo.c_str());
-
-        break;
+    // 1 - ler argv
+    // 2 - argv deve ter o caminho do arquivo .json
+    if (argc < 2) {
+        std::cerr << "Error: .json file path not provided.\n";
+        std::cerr << "Usage: " << argv[0] << " <path_to_content.json>\n";
+        return 1; // Finaliza o programa por erro de argumento
     }
 
+    std::filesystem::path filePath = argv[1];
 
+    // 3 - Se não existe exibir mensagem e finalizar 
+    if (!std::filesystem::exists(filePath)) {
+        std::cerr << "Error: The file '" << filePath.string() << "' does not exist.\n";
+        return 1; // Finaliza o programa pois o arquivo não foi encontrado
+    }
+
+    // 4 - Se existe, passar o arquivo do arquivo para BuildTask::OpenProject
+    // 5 - BuildTask::OpenProject(caminhoArquivo);
+    BuildTask::OpenProject(filePath.string());
+
+    // 6 - Finalizar o programa
     return 0;
 }
